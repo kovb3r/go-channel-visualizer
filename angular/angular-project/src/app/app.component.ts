23ab2e64e971fc6ca4021a2a131bd44222a4f203
kovb3r <bertakovanda@gmail.com>
2025-04-22 10:17:09 +0200

    d3 integralas

--- angular/angular-project/src/app/app.component.ts
+++ angular/angular-project/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { D3ExampleComponent } from './d3-example/d3-example.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, D3ExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
