f0f22211e26c263c462158fe5192ee776b0815b4
kovb3r <bertakovanda@gmail.com>
2025-06-27 11:48:59 +0200

    Message T tipus + kommentezes

--- example/example.go
+++ example/example.go
@@ -6,30 +6,77 @@ import (
 	"sync"
 )
 
-var wg sync.WaitGroup
+var wg sync.WaitGroup // WaitGroup a goroutine-ok befejeződésének megvárásához
 
-func ping(c wrapper.Channel) {
-    c.Send("ping")
-    msg := c.Receive()
-    fmt.Println(msg.Value)
-    wg.Done()
+// Saját típus: név és életkor
+type name struct {
+	Name string
+	Age  int
 }
 
-func pong(c wrapper.Channel) {
-    msg := c.Receive()
-    fmt.Println(msg.Value)
-    c.Send("pong") 
-    wg.Done()
+// Egyik goroutine, amely küld egy name típusú értéket, majd vár egy választ
+func nameChannel(c wrapper.Channel[name]) {
+	c.Send(name{"Alice", 30})
+	msg := c.Receive()
+	fmt.Printf("Received: %s, Age: %d\n", msg.Name, msg.Age)
+	wg.Done()
+}
+
+// Másik goroutine: fogadja az előzőtől kapott name értéket, majd visszaküld egy másikat
+func nameChannel2(c wrapper.Channel[name]) {
+	msg := c.Receive()
+	fmt.Printf("Received: %s, Age: %d\n", msg.Name, msg.Age)
+	c.Send(name{"Bob", 25})
+	wg.Done()
+}
+
+// Goroutine: string típusú üzenetküldés ("ping"), majd válasz fogadása
+func ping(c wrapper.Channel[string]) {
+	c.Send("ping")
+	msg := c.Receive()
+	fmt.Println(msg)
+	wg.Done()
+}
+
+// Goroutine: fogad egy string üzenetet, kiírja, majd válaszként "pong"-ot küld
+func pong(c wrapper.Channel[string]) {
+	msg := c.Receive()
+	fmt.Println(msg)
+	c.Send("pong")
+	wg.Done()
+}
+
+// Int típusú csatorna: küld egy számot, majd fogad egy választ
+func number(c wrapper.Channel[int]) {
+	c.Send(42)
+	msg := c.Receive()
+	fmt.Println(msg)
+	wg.Done()
+}
+
+// Int típusú csatorna másik fele: fogad, majd válaszol
+func number2(c wrapper.Channel[int]) {
+	msg := c.Receive()
+	fmt.Println(msg)
+	c.Send(24)
+	wg.Done()
 }
 
 func main() {
-    c := wrapper.CreateChannel()
-    b := wrapper.CreateChannel()
+	// Három típusos csatorna létrehozása: string, name és int
+	c := wrapper.CreateChannel[string]()
+	n := wrapper.CreateChannel[name]()
+	i := wrapper.CreateChannel[int]()
 
-    wg.Add(4)
-    go ping(c) 
-    go pong(c)
-    go ping(b)
-    go pong(b)
-    wg.Wait()
+	// 6 goroutine-t indítunk el
+	wg.Add(6)
+	go ping(c)
+	go pong(c)
+	go number(i)
+	go number2(i)
+	go nameChannel(n)
+	go nameChannel2(n)
+
+	// Várakozás az összes goroutine befejeződésére
+	wg.Wait()
 }
--- wrapper/main.go
+++ wrapper/main.go
@@ -12,68 +12,73 @@ import (
 	"time"
 )
 
-var channelCounter int      // egyedi azonosító a csatornákhoz
-var mu sync.Mutex
-var logFile string
-var messageCounter int // egyedi azonosító küldött üzenetekhez
+var channelCounter int    // egyedi azonosító a csatornákhoz
+var mu sync.Mutex        // szinkronizáláshoz használt mutex
+var logFile string      // log fájl neve
+var messageCounter int // egyedi azonosító üzenetekhez
 
+// A program indulásakor legenerál egy egyedi fájlnevet az eseménylog számára
 func init() {           
     timestamp := time.Now().Format("20060102_150405.000") // ÉvHónapNap_ÓraPercMásodperc.Millisec
-    logFile = fmt.Sprintf("channels_%s.json", timestamp)
-
-    if err := os.WriteFile(logFile, []byte("[]"), 0644); err != nil {
-        log.Fatalf("init log file: %v", err)
-    }
+    logFile = fmt.Sprintf("channels_%s.json", timestamp) // például: channels_20250627_114301.123.json
 }
 
-type Channel struct {
-    ID int
-    Chan chan Message
+//Általános csatorna típus generikus T típussal
+type Channel[T any] struct {
+    ID int  
+    Chan chan Message[T]
 }
 
-type Message struct {
-    SenderID int64  
-    ChannelID int
-    MessageID int
-    Value string
+// M
+type Message[T any] struct {
+    SenderID   int64 // küldő goroutine ID
+	ChannelID  int   // az adott csatorna azonosítója
+	MessageID  int   // az üzenet egyedi azonosítója
+	Value      T     // maga az üzenet (bármilyen típus lehet)
 }
 
-func CreateChannel() Channel {          //
-
+// Létrehoz egy új, típusos csatornát
+func CreateChannel[T any]() Channel[T] {          
+    //Kritikus szakasz, ahol növeljük a csatorna számlálót
     mu.Lock()
     channelCounter++
     id := channelCounter
     mu.Unlock()
 
+    // logoljuk a csatorna létrehozását
     appendJSON(map[string]interface{}{
         "channelId": id,
         "timestamp": time.Now().Format(time.RFC3339Nano),
     })
 
-    return Channel{
+    return Channel[T]{
         ID:   id,
-        Chan: make(chan Message),
+        Chan: make(chan Message[T]),
     }
 
 }
 
-func (c Channel) Send(value string) {
-    gid := GetGoid()
+// Üzenet küldése a csatornára, és esemény logolása
+func (c Channel[T]) Send(value T) {
+    gid := GetGoid() // küldő goroutine id-ja
+
+    // Kritikus szakasz, ahol növeljük az üzenet számlálót
     mu.Lock()
     messageCounter++
     id := messageCounter
     mu.Unlock()
 
-    message := Message{
+    message := Message[T]{
         SenderID:  gid,
         ChannelID: c.ID,
         MessageID: id,
         Value:     value,
     }
 
+    // esemény logolása
     appendJSON(map[string]interface{}{
         "event":       "send",
-        "goroutineId": gid,
+        "senderId": gid,
         "channelId":   c.ID,
         "messageId": id,
         "value":       value,
@@ -83,35 +88,32 @@ func (c Channel) Send(value string) {
     c.Chan <- message
 }
 
-func (c Channel) Receive() Message {
-    gid := GetGoid()
-    for {
-        msg := <-c.Chan
-        if msg.SenderID != gid {
-            appendJSON(map[string]interface{}{
-                "event":       "receive",
-                "goroutineId": gid,
-                "channelId":   c.ID,
-                "messageId":   msg.MessageID,
-                "value":       msg.Value,
-                "timestamp":   time.Now().Format(time.RFC3339Nano),
-            })
-            return msg
-        }
+func (c Channel[T]) Receive() T {
+    gid := GetGoid() // fogadó goroutine id-ja
 
-        // saját üzenet, visszatesszük, várunk
-        go func(m Message) {
-            time.Sleep(time.Millisecond)
-            c.Chan <- m
-        }(msg)
-    }
+    msg := <-c.Chan 
+        
+    // esemény logolása
+    appendJSON(map[string]interface{}{
+        "event":       "receive",
+        "recieverId": gid,
+        "channelId":   c.ID,
+        "messageId":   msg.MessageID,
+        "value":       msg.Value,
+        "timestamp":   time.Now().Format(time.RFC3339Nano),
+    })
+    return msg.Value
 }
 
+// Log bejegyzés hozzáadása a JSON fájlhoz
 func appendJSON(entry map[string]interface{}) {
+
     mu.Lock()
     defer mu.Unlock()
 
     var all []map[string]interface{}
+
+    // meglévő adatok beolvasása (ha már létezik a fájl)
     data, err := os.ReadFile(logFile)
     if err == nil && len(data) > 0 {
         if err := json.Unmarshal(data, &all); err != nil {
@@ -119,9 +121,10 @@ func appendJSON(entry map[string]interface{}) {
         }
     }
     
+    // új bejegyzés hozzáadása
     all = append(all, entry)
 
-    
+    // JSON formátumra alakítás és fájlba írás
     out, err := json.MarshalIndent(all, "", "  ")
     if err != nil {
         log.Fatalf("JSON marshal hiba: %v", err)
@@ -131,16 +134,20 @@ func appendJSON(entry map[string]interface{}) {
     }
 }
 
+// Goroutine ID lekérdezése (nem hivatalos módszer, de működik)
 func GetGoid() int64 {
     var (
-        buf [64]byte
-        n   = runtime.Stack(buf[:], false)
-        stk = strings.TrimPrefix(string(buf[:n]), "goroutine")
+        buf [64]byte // ide másoljuk be a goroutine stack header-t
+        n   = runtime.Stack(buf[:], false) // stack trace lekérése (false = csak aktuális goroutine)
+        stk = strings.TrimPrefix(string(buf[:n]), "goroutine") // eltávolítjuk a "goroutine" szöveget az elejéről
     )
 
+    // A maradék szöveg első szóközig tartó része a goroutine ID pl. "123 [running]:"
     idField := strings.Fields(stk)[0]
+
+    // Konvertáljuk a szöveget számmá (pl. "123" → 123)
     id, err := strconv.Atoi(idField)
-    if err!= nil {
+    if err != nil {
         panic(fmt.Errorf("can not get goroutine id: %v", err))
     }
 
